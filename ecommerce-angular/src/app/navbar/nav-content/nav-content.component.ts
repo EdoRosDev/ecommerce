34740afd39840d0606a2521f511a58466b1b7e0e
EdoRosDev <edorosiellodev@gmail.com>
2024-02-02 16:57:26 +0100

    navbar component

--- ecommerce-angular/src/app/navbar/nav-content/nav-content.component.ts
+++ ecommerce-angular/src/app/navbar/nav-content/nav-content.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { navigation } from '../../../data/navbarContent';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'app-nav-content',
+  selector: 'nav-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './nav-content.component.html',
   styleUrl: './nav-content.component.scss'
 })
-export class NavContentComponent {
+export class NavContentComponent implements OnInit {
+  
+  category: any;
+  @Input() selectedSection: any;
+  
+  ngOnInit(): void {
+    this.category = navigation
+  }
 
 }
--- ecommerce-angular/src/app/navbar/navbar.component.ts
+++ ecommerce-angular/src/app/navbar/navbar.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon'
 import {MatButtonModule} from '@angular/material/button'
 import {MatMenuModule} from '@angular/material/menu';
 import { CommonModule } from '@angular/common';
+import { NavContentComponent } from './nav-content/nav-content.component';
 
 @Component({
   selector: 'navbar',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatMenuModule, CommonModule],
+  imports: [MatIconModule, MatButtonModule, MatMenuModule, CommonModule, NavContentComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   logged: boolean = false;
+  isNavbarContentOpen: boolean = false;
+  currentSection: string = ''
 
   login(){
     this.logged = true
@@ -26,12 +29,36 @@ export class NavbarComponent {
     return this.logged
   }
 
-  openNavbarContent(menu: string){
+  openNavbarContent(section: string){
+    this.currentSection = section
+    this.isNavbarContentOpen = true;
+  }
 
+  closeNavbarContent(){
+    this.currentSection = ''
+    this.isNavbarContentOpen = false;
   }
 
   navigateTo(path:string){
 
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent){
+    const modalContainer = document.querySelector(".modal-container");
+    const openButtons = document.querySelectorAll(".open-button");
+
+    let clickInsideButton = false;
+
+    openButtons.forEach((button:Element) => {
+      if(button.contains(event.target as Node)){
+        clickInsideButton = true;
+      }
+    })
+
+    if(modalContainer && !clickInsideButton && this.isNavbarContentOpen){
+      this.closeNavbarContent();
+    }
   }
 
 }
