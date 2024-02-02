290791a5a036842f024ce3f7276a6143e23f4d8d
EdoRosDev <edorosiellodev@gmail.com>
2024-02-02 16:58:00 +0100

    Added footer on main page

--- ecommerce-angular/src/app/app.component.ts
+++ ecommerce-angular/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
